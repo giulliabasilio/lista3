437e65af37ff3a4b9b3a54c44b2715eee88d4403
giulliabasilio <giulliabasilio@gmail.com>
2025-06-04 17:24:42 -0300

    Add files via upload

---
+++ lista3ex8/lista3ex8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            int i = 1;
+
+            while (i <= 100)
+            {
+                soma = soma + i;
+                i = i + 1;
+            }
+
+            Console.WriteLine("A soma de 1 até 100 é: " + soma);
+        }
+    }
+}
+    
---
+++ lista3ex9/lista3ex9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int cont = 1;
+
+            Console.WriteLine(a);
+
+            while (cont < 30)
+            {
+                Console.WriteLine(b);
+                int temp = a + b;
+                a = b;
+                b = temp;
+                cont = cont + 1;
+            }
+        }
+    }
+}
