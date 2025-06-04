35554934734eeb3129650deff46a8ccd332c87d8
giulliabasilio <giulliabasilio@gmail.com>
2025-06-04 17:07:51 -0300

    Add files via upload

---
+++ lista3ex3/lista3ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo = "";
+
+            while (sexo != "F" && sexo != "M")
+            {
+                Console.Write("Digite o sexo (F ou M): ");
+                sexo = Console.ReadLine();
+
+                if (sexo != "F" && sexo != "M")
+                {
+                    Console.WriteLine("Valor inválido.");
+                }
+            }
+
+            Console.WriteLine("Sexo digitado: " + sexo);
+        }
+    }
+}
