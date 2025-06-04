0f95789892c1f7dd90157f218a5285cf4d3635fe
giulliabasilio <giulliabasilio@gmail.com>
2025-06-04 17:21:50 -0300

    Add files via upload

---
+++ lista3ex7/lista3ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            while (num <= 20)
+            {
+                int i = 1;
+                while (i <= 10)
+                {
+                    Console.WriteLine(num + " x " + i + " = " + (num * i));
+                    i = i + 1;
+                }
+
+                Console.WriteLine("Pressione uma tecla para continuar...");
+                Console.ReadKey();
+
+                num = num + 1;
+            }
+        }
+    }
+}
+    
