bbaeca5b0880cc1de88d0f20071f34ee7a899c56
giulliabasilio <giulliabasilio@gmail.com>
2025-06-04 17:19:56 -0300

    Add files via upload

---
+++ lista3ex5/lista3ex5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                 int valor = -1;
+
+            while (valor <= 0)
+            {
+                Console.Write("Digite um valor positivo: ");
+                valor = int.Parse(Console.ReadLine());
+
+                if (valor <= 0)
+                {
+                    Console.WriteLine("Valor inválido!");
+                }
+            }
+
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(valor + " x " + i + " = " + (valor * i));
+                i = i + 1;
+            }
+        }
+    }
+}
+
---
+++ lista3ex6/lista3ex6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -1;
+            while (x <= 0)
+            {
+                Console.Write("Digite um valor positivo para X: ");
+                x = int.Parse(Console.ReadLine());
+                if (x <= 0)
+                {
+                    Console.WriteLine("Valor inválido!");
+                }
+            }
+
+            Console.Write("Digite o valor A (início do intervalo): ");
+            int a = int.Parse(Console.ReadLine());
+
+            int b;
+            Console.Write("Digite o valor B (fim do intervalo, maior que A): ");
+            b = int.Parse(Console.ReadLine());
+
+            while (b <= a)
+            {
+                Console.WriteLine("B deve ser maior que A!");
+                Console.Write("Digite o valor B novamente: ");
+                b = int.Parse(Console.ReadLine());
+            }
+
+            int i = b;
+            while (i >= a)
+            {
+                Console.WriteLine(x + " x " + i + " = " + (x * i));
+                i = i - 1;
+            }
+        }
+    }
+}
